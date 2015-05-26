a832fca03562e0ab6c5b91b64217d9c5a5943ac2
secret8060 <91799897@naver.com>
2015-05-26 15:59:41 +0900

    vo.6

--- taomTestProjectjava/src/taomTestProjectjava/Test.java
+++ taomTestProjectjava/src/taomTestProjectjava/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test1");
 		System.out.println("최지우 수정1");
-		System.out.println("박영훈 수정2");
+		System.out.println("박영훈 수정2 -  수정");
 	}
 }
